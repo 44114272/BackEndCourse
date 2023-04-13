cd74b50c13f1a92ee7464993c0c1ad09edec747c
Joaquín Elía <joaquin.elia@hotmail.com>
2023-04-13 16:40:38 -0300

    Challenge three updated

--- classThree/src/app.js
+++ classThree/src/app.js
@@ -21,26 +21,4 @@ app.get('/products/:id', async(req, res) => {
     else {res.send('Product not found')};
 })
 
-
 app.listen(8080, () => console.log("Listening on 8080"));
-
-
-
-
-
-
-
-
-
-
-
-// await manager.addProduct('Test product 1', "This is a test product 1", 100, "Without image", "abc123", 25);
-// await manager.addProduct('Test product 2', "This is a test product 2", 200, "Without image", "abc456", 25);
-// await manager.addProduct('Test product 3', "This is a test product 3", 300, "Without image", "abc789", 25);
-// await manager.addProduct('Test product 4', "This is a test product 4", 400, "Without image", "def123", 25);
-// await manager.addProduct('Test product 5', "This is a test product 5", 500, "Without image", "def456", 25);
-// await manager.addProduct('Test product 6', "This is a test product 6", 600, "Without image", "def789", 25);
-// await manager.addProduct('Test product 7', "This is a test product 7", 700, "Without image", "ghi123", 25);
-// await manager.addProduct('Test product 8', "This is a test product 8", 800, "Without image", "ghi456", 25);
-// await manager.addProduct('Test product 9', "This is a test product 9", 900, "Without image", "ghi789", 25);
-// await manager.addProduct('Test product 10', "This is a test product 10", 1000, "Without image", "jkl123", 25);
