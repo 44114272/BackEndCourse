5939475898d84b74dcf0f08aa1835b1a49c95f52
Joaquín Elía <joaquin.elia@hotmail.com>
2023-07-15 22:05:39 -0300

    Adding ticket model

--- FinalProjectThree/src/dao/dbManagers/ticket.dao.js
+++ FinalProjectThree/src/dao/dbManagers/ticket.dao.js
@@ -1,4 +1,4 @@
-import { ticketModel } from "../models/ticket.model.js";
+import { ticketModel } from "../models/ticket.js";
 import { v4 as uuidv4 } from 'uuid';
 
 export default class TicketManagerDB {
